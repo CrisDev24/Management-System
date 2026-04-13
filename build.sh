a1d0da0875b5cbfb80e1371ace2f1269673947e2
CrisDev24 <cristianley2202@gmail.com>
2026-04-13 11:15:00 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,3 +6,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+DJANGO_SUPERUSER_PASSWORD = 120321
+
+python manage.py createsuperuser --noinput --username admin --email cristianahl08@gmail.com
